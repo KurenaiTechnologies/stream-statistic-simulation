8b4584955fc98bb2513e3da9042249a244c5ccd3
PayToUse <67496494+PayToUse@users.noreply.github.com>
2023-08-05 19:03:59 +0800

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -12,22 +12,26 @@ let sMessages = Math.floor(Math.random() * 750000) + (x * 300);
 let sFollowers = Math.floor(Math.random() * 12500) + (x * 250);
 let sSubs = Math.floor(Math.random() * 2500) + (x * 100);
 
+// Add the revenue from subs with the total revenue
+var subsRevenue = 5;
+var totalSubRevenue = sSubs * subsRevenue;
+sRevenue+=totalSubRevenue;
+
 // set an interval that executes some functions here every second
 setInterval(() => {
     
-    var uViews = Math.floor(Math.random() * 1000) + (x * 150);
+    var uViews = Math.floor(Math.random() * 1250) + (x * 175);
     var uRevenue = Math.floor(Math.random() * 5) + (x * 15);
     var uFollowers = Math.floor(Math.random() * 3) + (x * 7);
     var uMessages = Math.floor(Math.random() * 15) + (x * 5);
     var uSubs = Math.floor(Math.random() * 5) + (x * 3);
-    var subsRevenue = 5;
-    var totalSubRevenue = sSubs * subsRevenue;
+    var uSubRevenue = uSubs * 5;
     
     sViews+=uViews;
     sRevenue+=uRevenue;
     sFollowers+=uFollowers;
     sMessages+=uMessages;
-    sRevenue+=totalSubRevenue;
+    totalSubRevenue+=uSubRevenue;
     sSubs+=uSubs;
     sTime+=1;
     x+=1;
@@ -41,13 +45,14 @@ setInterval(() => {
     document.getElementById("totalfollowers").innerHTML = abbreviateNumber(sFollowers);
     document.getElementById("overalltime").innerHTML = time;
     document.getElementById("totalsubs").innerHTML = abbreviateNumber(sSubs);
+    document.getElementById("totalsubrevenue").innerHTML = abbreviateNumber(uSubRevenue)
     
     document.getElementById("viewers").innerHTML = abbreviateNumber(uViews) + " Watching";
     document.getElementById("timedisplay").innerHTML = timeConvert(sTime)
 
 }, 1000)
 
-// function for abbreviating numbers with suffixes, for example: 1e15 = 1Qd
+// Make a function for abbreviating numbers with suffixes, for example: 1e15 = 1Qd
 function abbreviateNumber(value) {
     let abbreviatedValue = value;
     const letters = ["", "K", "M", "B","T", "Qd", "Qn", "Sx", "Sp", "Oc", "No", "De", "UDe", "DDe", "TDe", "QdDe", "QnDe", "SxDe", "SpDe", "OcDe", "NoDe", "Vg", "UVg", "DVg", "TVg", "QdVg", "QnVg", "SxVg", "SpVg", "OcVg", "NoVg", "Tg", "UTg", "DTg", "TTg", "QdTg", "QnTg", "SxTg", "SpTg", "OcTg", "NoTg"];
@@ -65,7 +70,7 @@ function abbreviateNumber(value) {
 }
 
 
-// function to convert any value to time
+// Make a function to convert any value to time
 function timeConvert(value) {
     var minutes = Math.floor(value / 60);
     var seconds = value % 60;
@@ -80,7 +85,7 @@ function timeConvert(value) {
     return minutes + ":" + seconds;
 }
 
-// function for real-time clock
+// Make a function for real-time clock
 function startTime() {
   const today = new Date();
   let h = today.getHours();
@@ -92,13 +97,13 @@ function startTime() {
   setTimeout(startTime, 1);
 }
 
-// add 0 to values below 10 for the clock function
+// Add 0 to values below 10 for the clock function
 function checkTime(i) {
   if (i < 10) {i = "0" + i}; 
   return i;
 }
 
-// a function that prompts a window where you setup two components; description and title once the page has opened
+// Make a function that prompts a window where you setup three components; description, streamer name and title once the page has been opened
 function streamSetup() {
     let streamName = prompt("Please enter your stream name (optional)");
     
